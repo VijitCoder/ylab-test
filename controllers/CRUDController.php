655236ea9714b363225d04d432122e538d008a21
Vijit <vijit@mail.ru>
2018-07-21 20:17:06 +0700

    Replace CRUD controllers with one super controller.

---
+++ controllers/CRUDController.php
@@ -0,0 +1,160 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\base\UnknownMethodException;
+use yii\db\ActiveRecord;
+use yii\db\StaleObjectException;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * Base CRUD controller, implements the CRUD actions for any model.
+ */
+abstract class CRUDController extends Controller
+{
+    /**
+     * Get entity model
+     *
+     * @return ActiveRecord
+     */
+    abstract protected function getModel(): ActiveRecord;
+
+    /**
+     * Get search entity model
+     *
+     * @return ActiveRecord
+     */
+    abstract protected function getSearchModel(): ActiveRecord;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class'   => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all entity models.
+     *
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws UnknownMethodException
+     */
+    public function actionIndex()
+    {
+        $searchModel = $this->getSearchModel();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel'  => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single entity model.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new entity model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     *
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function actionCreate()
+    {
+        $model = $this->getModel();
+        return $this->renewEntity($model, 'create');
+    }
+
+    /**
+     * Updates an existing entity model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws InvalidArgumentException
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        return $this->renewEntity($model, 'update');
+    }
+
+    /**
+     * Update or create new entity model.
+     *
+     * @param ActiveRecord $model
+     * @param string       $view update|create
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    private function renewEntity(ActiveRecord $model, string $view)
+    {
+        $data = Yii::$app->request->post();
+
+        if ($model->load($data) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render($view, ['model' => $model]);
+    }
+
+    /**
+     * Deletes an existing entity model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     * @throws \Throwable
+     * @throws StaleObjectException
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the entity model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     *
+     * @param integer $id
+     * @return ActiveRecord the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel(int $id): ActiveRecord
+    {
+        $model = $this->getModel()->findOne($id);
+        if ($model) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,130 +1,32 @@
 <?php
-
 namespace app\controllers;
 
-use Yii;
 use app\models\Category;
 use app\models\CategorySearch;
-use yii\base\InvalidArgumentException;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
+use yii\db\ActiveRecord;
 
 /**
  * CategoryController implements the CRUD actions for Category model.
  */
-class CategoryController extends Controller
+class CategoryController extends CRUDController
 {
     /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * Lists all Category models.
+     * Get Category model
      *
-     * @return mixed
-     * @throws InvalidArgumentException
+     * @return ActiveRecord
      */
-    public function actionIndex()
+    protected function getModel(): ActiveRecord
     {
-        $searchModel = new CategorySearch;
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+        return new Category;
     }
 
     /**
-     * Displays a single Category model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
+     * Get category search model
+     *
+     * @return ActiveRecord
      */
-    public function actionView($id)
+    protected function getSearchModel(): ActiveRecord
     {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Category model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Category;
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Category model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Category model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Category model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Category the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Category::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
+        return new CategorySearch;
     }
 }
